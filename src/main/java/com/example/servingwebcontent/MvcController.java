d42243e6931bf29fd2662a26e61fd678ff77685f
Vlad <naumkin.vlad2012@yandex.ru>
2021-05-07 12:39:08 +0300

    Select Spring WEB, Spring Data JPA, MySQL Driver. Create DataBase test.

--- src/main/java/com/example/servingwebcontent/MvcController.java
+++ src/main/java/com/example/servingwebcontent/MvcController.java
@@ -1,18 +1,36 @@
 package com.example.servingwebcontent;
 
+import com.example.servingwebcontent.repos.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MvcController {
+    @Autowired
+    private MessageRepo messageRepo;
+
+    /*@PostMapping
+    public String addNewMessage (@RequestParam String text, Model model){
+        Message message = new Message();
+        message.setText(text);
+        messageRepo.save(message);
+
+        Iterable<Message> messages = messageRepo.findAll();
+        model.addAttribute("text", text);
+        return "Saved";
+    }*/
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
     }
+
 
     @GetMapping("/bye")
     public String bye(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
