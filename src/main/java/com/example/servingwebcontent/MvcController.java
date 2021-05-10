2ff86b69e486f901c04d33f4df5a0eeebb201119
Vlad <naumkin.vlad2012@yandex.ru>
2021-05-10 12:14:48 +0300

    Work on Post request.

--- src/main/java/com/example/servingwebcontent/MvcController.java
+++ src/main/java/com/example/servingwebcontent/MvcController.java
@@ -7,14 +7,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping(path="/demo") // This means URL's start with /demo (after Application path)
+//@RequestMapping(path="/main") // This means URL's start with /demo (after Application path)
 
 public class MvcController {
     @Autowired
     private MessageRepo messageRepo;
 
-
-    @GetMapping(path="/all")
+    @GetMapping("/all")
     public String all(Model model) {
         Iterable<Message> messages = messageRepo.findAll();
         model.addAttribute("messages", messages);
@@ -33,11 +32,14 @@ public class MvcController {
         return "bye";
     }
 
-    @PostMapping
-    public String addNewMessage (@RequestParam String text){
+    @PostMapping("/add")
+    public String addNewMessage (@ModelAttribute("text") String text){
+        System.out.println(text);
         Message message = new Message();
         message.setText(text);
         messageRepo.save(message);
+        System.out.println("Yes");
         return "index";
     }
+
 }
